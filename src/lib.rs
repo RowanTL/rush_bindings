6b19425f38470bdccfe5cec5b17a47fafefe59cb
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-09-21 16:24:33 -0500

    Have an iterator over a list

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn rs_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
+mod rush_bindings {
+    use pyo3::{prelude::*, types::PyList};
+
+    #[pyfunction]
+    fn add(x: Bound<'_, PyList>) -> usize {
+        for item in x.iter() {
+            println!("{:?}", item);
+        }
+        1
+    }
 }
