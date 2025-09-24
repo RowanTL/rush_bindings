607f8b0ed05b8f0f5410b97dd0b6d504b3fc3e46
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-09-23 22:36:40 -0500

    more testing and such

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,27 @@
 use pyo3::prelude::*;
 
+const CODE_BLOCK_STR: &str = "<class 'pyshgp.push.atoms.CodeBlock'>";
+
 #[pymodule]
 mod rush_bindings {
     use pyo3::{prelude::*, types::PyDict};
-    use rush::push::state::PushState;
+    use rush::push::state::{Gene, PushState};
 
     /// Later, this function will need to accept inputs in its own
     /// parameter.
+    /// Can kinda just take the module as input. Cool.
     #[pyfunction]
-    fn pront(x: Bound<'_, PyDict>) -> PyResult<()> {
+    fn pront(x: Bound<'_, PyDict>, py: Python<'_>) -> PyResult<()> {
+        // println!("{:?}", x.get_type());
+        // println!("{:?}", x.get_item("exec")?.unwrap().get_type().to_string());
         let rust_state: PushState = x.extract()?;
-        // let genome = x.get_item("exec")?.unwrap();
-        // for gene in &(genome.try_iter()) {}
-        // if let Ok(genome_iter) = &(genome.try_iter()) {
+        let genome = x.get_item("exec")?.unwrap();
+        rec_block(&genome)?;
         //     for gene in genome_iter {
         //         let temp_gene = &gene?;
         //         println!("{:?}", temp_gene);
         //         if let Ok(block) = temp_gene.try_iter() {
-        //             println!("block found");
+        //             println!("{:?}", );
         //         }
         //         let temp_str = temp_gene.to_string();
         //         if temp_str.starts_with("InstructionMeta") {
@@ -30,5 +34,16 @@ mod rush_bindings {
         //     }
         // }
         Ok(())
+    }
+
+    fn rec_block(block: &Bound<'_, PyAny>) -> PyResult<Vec<Gene>> {
+        let block_iter = block.try_iter()?;
+        for gene in block_iter {
+            let temp_gene = gene?;
+            if temp_gene.to_string().starts_with("InstructionMeta") {
+                println!("{:?}", temp_gene.getattr("name")?.to_string());
+            }
+        }
+        Ok(Vec::new())
     }
 }
